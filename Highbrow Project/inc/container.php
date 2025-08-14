6a29da54f99f5124d1885f2f0a1921a2276f85e2
http <godwinebito833@gmail.com>
2025-08-14 04:06:13 +0100

    Add files via upload

---
+++ Highbrow Project/inc/container.php
@@ -0,0 +1,16 @@
+</head>
+<body class="">
+<div class="container" style="min-height:500px;">
+	
+
+
+Create a file called footer.php inside the inc folder
+
+Type the following code inside the footer.php file
+
+
+<div class="insert-post-ads1" style="margin-top:20px;">
+
+</body>
+</html>
+
